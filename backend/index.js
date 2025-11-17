834e5d186b23f9354ff1bcf765144bbdbafb8c03
Ying Jie Mei <jackymei95@gmail.com>
2025-11-17 13:02:29 -0500

    connected to database

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
+import connectDB from './db.js'
 
 const app = express()
 
@@ -14,4 +15,5 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
     console.log('Listening on port: ' + port)
+    connectDB()
 })
