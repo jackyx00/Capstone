8192cb2d237718b732fedcc9e4f1dd016ee8998d
Ying Jie Mei <jackymei95@gmail.com>
2025-11-17 12:00:22 -0500

    backend basic setup

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express()
+
+const port = 8080
+
+app.get('/', (req, res) => {
+    res.json('Hello from server')
+})
+
+app.listen(port, () => {
+    console.log('Listening on port: ' + port)
+})
