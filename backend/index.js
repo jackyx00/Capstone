d3208b71e4ef51daea16422fdb1217974f60c2d8
Ying Jie Mei <jackymei95@gmail.com>
2025-11-17 12:22:19 -0500

    connected backend and frontend

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,12 @@
 import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
 
 const app = express()
 
-const port = 8080
+const port = process.env.PORT
+
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.json('Hello from server')
