0631d0f991e9ab3e88cf5b527a305b7a97418795
Ying Jie Mei <jackymei95@gmail.com>
2025-11-21 09:22:33 -0500

    run setup backend once

--- backend/index.js
+++ backend/index.js
@@ -2,22 +2,23 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import connectDB from './db.js';
-import pokemonSetupRoute from './routes/pokemonSetup.js'
+import runPokemonSetup from './pokemonSetup.js'
 
 const app = express()
-
 const port = process.env.PORT
 
 app.use(cors())
 app.use(express.json())
 
-connectDB()
+connectDB().then(() => {
+    runPokemonSetup();
+})
 
 app.get('/', (req, res) => {
     res.json('Hello from server')
 })
 
-app.use("/pokemon", pokemonSetupRoute)
+// app.use("/pokemon", pokemonSetupRoute)
 
 app.listen(port, () => {
     console.log('Listening on port: ' + port)
